28ef5077fd2334496f57e2c9d2da7fb456b60d6c
Edson_yuri <edsonyuri2014@hotmail.com>
2024-05-22 23:59:49 -0300

    feat: seleção de planetas e suas acelerações

--- script.js
+++ script.js
@@ -1,10 +1,23 @@
 const nave = document.getElementById('nave')
+const body = document.querySelector('body')
 let velocidade = 0
+let aceleracaoGravidade = 0;
+let aceleracao = 0;
 
-let tempo = Date.now()
+const planetaSelecionado = document.getElementsByClassName('planetas')
 
-// const aceleracao = -3 * (10 ** (-3)) // aceleração = 300px por segundo ao quadrado
-const aceleracao = -24 * 300 / (1000) ** 2 // aceleração = 1.6 * 300px por segundo ao quadrado
+for (let i = 0; i <= planetaSelecionado.length - 1; i++) {
+    planetaSelecionado[i].addEventListener('click', () => {
+        aceleracaoGravidade = Number(planetaSelecionado[i].value)
+        body.style.backgroundImage = `url('assets/${planetaSelecionado[i].textContent}.jpg')`
+
+        aceleracao = (-aceleracaoGravidade * 300 / (1000) ** 2) / 10 // recalcular a aceleração aqui
+        velocidade = 0
+        nave.style.bottom = '600px'
+
+        simulaQueda()
+    })
+}
 
 const RecuperaAlturaDaNave = () => {
     const { bottom } = getComputedStyle(nave) //recupera o valor da propriedade bottom da nave
@@ -16,18 +29,24 @@ const RecuperaAlturaDaNave = () => {
 //S = S0 + vt + 0.5 * at^2
 const novaAlturaDaNave = (S0, v, t) => S0 + v * t + 0.5 * aceleracao * (t ** 2) // calcula a nova altura da nave
 
-setInterval(() => {
-    let tempoAtual = Date.now()
-    let intervaloDeTempo = tempoAtual - tempo
-    tempo = tempoAtual
+function simulaQueda() {
+    let tempo = Date.now()
 
-    console.log(intervaloDeTempo)
+    const intervalo = setInterval(() => {
+        let tempoAtual = Date.now()
+        let intervaloDeTempo = tempoAtual - tempo
+        tempo = tempoAtual
 
-    const altura = RecuperaAlturaDaNave()
+       // console.log(intervaloDeTempo)
 
-    velocidade += aceleracao * intervaloDeTempo // atualiza a velocidade 
+        const altura = RecuperaAlturaDaNave()
 
-    const novaAltura = novaAlturaDaNave(altura, velocidade, intervaloDeTempo) // calcula a nova altura
+        velocidade += aceleracao * intervaloDeTempo // atualiza a velocidade 
 
-    nave.style.bottom = `${Math.max(novaAltura, 0)}px` // atualiza a altura da nave no estilo, não permitindo que ela vá abaixo de 0
-}, 1)
+        const novaAltura = novaAlturaDaNave(altura, velocidade, intervaloDeTempo) // calcula a nova altura
+
+        nave.style.bottom = `${Math.max(novaAltura, 0)}px` // atualiza a altura da nave no estilo, não permitindo que ela vá abaixo de 0
+        if (nave.style.bottom == '0px') clearInterval(intervalo)
+    }, 1)
+    console.log('re')
+}
